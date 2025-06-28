5082b8ba0d6dfd618c491ff309b77667c02b3896
microwave <maximtour1@gmail.com>
2025-06-28 16:43:43 +1000

    added approximates of the first derivative of the moving averages, cleaned some code

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
 from datetime import datetime, timedelta
 import pandas as pd
 import mplfinance as mpf
+import numpy as np
 
-API_KEY = "xxxx"
-SECRET = "xxxx"
+API_KEY = ""
+SECRET = ""
 
 TARGET_SYMBOL = "SOXL"
 SHORT_MA_PERIOD = 8
 LONG_MA_PERIOD = 30
+MIN_SHORT_DELTA = 0.007  # Minimum short delta to consider a buy signal
+MAX_SHORT_DELTA = 0.015
+MAX_MA_DISTANCE = 0.02  # Maximum distance between short and long MA to consider a buy signal
 
 # REAL TIME DATA 
 def start_quote_stream(api_key, secret_key, symbol):
@@ -51,9 +55,35 @@ def get_historical_data(api_key, secret_key, symbol, startdate=None, endDate=Non
     print("Historical data retrieved")
     return data
 
+def get_long_posiitions(data, longWindow=LONG_MA_PERIOD):
+    # Identify long positions
+    long_positions = [0] * len(data)
+    for i in range(longWindow, len(data)):
+        if (data['Short MA'].iloc[i] > data['Long MA'].iloc[i] and
+            data['Short MA'].iloc[i - 1] >= data['Long MA'].iloc[i - 1]):
+            long_positions[i] = 1  # Long position entry
+
+    return long_positions
+
+def get_short_deltas(data):
+    # Calculate short deltas (first difference)
+    short_deltas = data['open'].diff()
+    return short_deltas
+    
+    
+def get_buy_signal(data):
+    # Check for buy signal
+    buy = [0] * len(data)
+    for i in range(LONG_MA_PERIOD, len(data)):
+        if data['Long Positions'].iloc[i] == 1 and data['Short Deltas'].iloc[i] > MIN_SHORT_DELTA and data['Short Deltas'].iloc[i] < MAX_SHORT_DELTA:
+            buy[i] = 1
+    return buy
+
+
+
 # MAIN EXECUTION
 def main():
-    historical_data = get_historical_data(API_KEY, SECRET, TARGET_SYMBOL, datetime.now() - timedelta(days=2), datetime.now() - timedelta(days=1))
+    historical_data = get_historical_data(API_KEY, SECRET, TARGET_SYMBOL, datetime.now() - timedelta(days=3), datetime.now() - timedelta(days=0.5))
 
     if historical_data is None:
         return
@@ -64,9 +94,47 @@ def main():
 
     historical_data['Short MA'] = historical_data['close'].rolling(window=SHORT_MA_PERIOD).mean()
     historical_data['Long MA'] = historical_data['close'].rolling(window=LONG_MA_PERIOD).mean()
+    
     MAshort = mpf.make_addplot(historical_data['Short MA'], color='blue', width=1, panel=0)
     MAlong = mpf.make_addplot(historical_data['Long MA'], color='red', width=1, panel=0)
-    
+
+    historical_data['Long Positions'] = get_long_posiitions(historical_data)
+    historical_data['Short Deltas'] = get_short_deltas(historical_data)
+
+    max_high = historical_data['high'].max()
+
+    # Highlight green markers where 'Long Positions' == 1
+    long_entry_prices = pd.Series(np.nan, index=historical_data.index)
+    long_entry_prices[historical_data['Long Positions'] == 1] = max_high
+
+    # Highlight growing deltas WHEN short ma is above long ma
+    historical_data['Buy Signal'] = get_buy_signal(historical_data)
+
+    # Plot blue markers at buy signal points (at close price)
+    buy_signal_prices = pd.Series(np.nan, index=historical_data.index)
+    buy_signal_prices[historical_data['Buy Signal'] == 1] = historical_data['open']
+
+    buy_markers = mpf.make_addplot(
+        buy_signal_prices,
+        type='scatter',
+        markersize=50,
+        marker='o',
+        color='blue',
+        panel=0
+    )
+
+
+
+    long_markers = mpf.make_addplot(
+        long_entry_prices,
+        type='scatter',
+        markersize=100,
+        marker='s',
+        color='green',
+        panel=0
+    )
+    # Export historical data to CSV
+    historical_data.to_csv(f"{TARGET_SYMBOL}_historical_data.csv")
 
     # Rename columns to match mplfinance
     historical_data.rename(columns={
@@ -83,7 +151,7 @@ def main():
         type='candle',
         style='charles',
         title=f"{TARGET_SYMBOL} Candlestick Chart",
-        addplot=[MAshort, MAlong],
+        addplot=[MAshort, MAlong, long_markers, buy_markers],
         volume=False
     )
 
